b2244be4c191c1f997fc0fe6f00b7ccca1c5f242
Jake <jakewarrenblack01@gmail.com>
2021-10-27 00:37:08 +0100

    Add search functionality to NavBar. No error checking on this yet. Downgrade Bootstrap to v4.5.3 attempting to add toast to NavBar.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,15 +8,15 @@
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
          <b-nav-item href="#"> <router-link :to="{name: 'home'}">Home</router-link></b-nav-item>
-        <b-nav-item href="#"><router-link :to="{name: 'all_countries'}">All</router-link></b-nav-item>
+          <b-nav-item href="#"><router-link :to="{name: 'all_countries'}">All</router-link></b-nav-item>
         
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ms-auto me-5">
         <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          <b-form-input v-model="search_term" v-on:keyup.enter="search()" size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button @click="search()" size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
         </b-nav-form>
       </b-navbar-nav>
     </b-collapse>
@@ -26,7 +26,27 @@
 
 <script>
     export default{
-        name: 'NavBar'
+        name: 'NavBar',
+        data(){
+          return{
+            search_term: ''
+          }
+        },
+        methods:{
+          search(){
+            //  if(!this.term){
+            //     this.$bvToast.toast('Please enter a search term.', {
+            //         title: 'Warning',
+            //         variant: 'danger',
+            //         toaster: 'b-toaster-top-center',
+            //         'autoHideDelay':5000,
+            //         solid: true                    
+            //     })
+            //     return;
+            //   }else
+              this.$router.push({name: 'country', params: { country: this.search_term}})
+          }
+        }
     }
 </script>
 
